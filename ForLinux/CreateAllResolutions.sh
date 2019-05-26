4b13b2c0d17d8eb0def49eed4613c2a29612234e
SunbossRS <yaorizhuangzhu@163.com>
2019-05-26 17:11:37 +0800

    make ForLinux makeresolutions[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C-[C[C[C[C[C[D[D[D[D[Dresolutions script

---
+++ ForLinux/CreateAllResolutions.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This file can let linux user add more resolutions. But the linux user need to have "xrandr", "cvt" command first. Ah, origin linux resolution is only have "800x600" "1024x768" resolutions! But some users can't adapt it. So a large resolution can improve the linux environment.
+# You can put this script to "Desktop" or not.
+# Go to https://github.com/sunbossrs/easierworkflow for download more EasierScript.
+
+echo -e "    ================\n   | CreateAll      |\n   | Resolutions !  |\n   |                |\n   |                |\n    ================\n	      |   |\n	  =============\n\n=============================\n\nThe script is going to create some resolutions."
+Modeline "1280x600_75.00"   78.25  1280 1344 1472 1664  600 603 613 629 -hsync +vsync
