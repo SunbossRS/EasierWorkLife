b133cd36a576dec7425479c1a79a1f49143c1e1b
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-16 00:33:36 +0800

    Update folderfree.sh

--- folderfree.sh
+++ folderfree.sh
@@ -1,6 +1,6 @@
 # !/bin/sh
-# This is the LinuxShell script for GitUpload.
+# This is the LinuxShell script for unlock GitUpload's put-folder.
 # ATTENTION! PUT THIS FILE TO *YourProject*/$gitflow/
 # Go to https://github.com/sunbossrs/gitflow/ for more EasierScript.
 
-echo "Finding the GitUpload file ..." && grep -q "cd .." GitUpload.sh && echo "Found GitUpload file." && sed -i 's/cd .. &&//g' GitUpload.sh || echo "Not found." && echo "Edit successful."
+echo "[+] Finding GitUpload ..." && grep -q "cd .." GitUpload.sh && echo -e "[+] Found GitUpload file.\n[+] Start modifying ..." && sed -i 's/cd .. && //g' GitUpload.sh || echo "[+] Not found." && echo "[+] The GitUpload have been modified."
