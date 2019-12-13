8de4cf1bbc43fd8883f3510c218e2a9157876594
SunbossRS <yaorizhuangzhu@163.com>
2019-12-13 22:54:49 +0800

    commit

--- GitUpload_DateTime.sh
+++ GitUpload_DateTime.sh
@@ -2,16 +2,4 @@
 # ATTENTION! PUT THIS FILE 2 *YOUR PROJECT*/$gitflow
 # Visit https://github.com/sunbossrs/gitflow for more infos.
 
-DATE=$(date -d'1 day ago' "+%Y-%m-%d")
-TIME=$(date "+%H:%M:%S")
-echo "  ____"
-echo " |"
-echo "   The upload-to-git-platform script as date-time format commit"
-echo "                                                             ____| "
-echo "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"
-echo ""
-echo "Commit changes ..."
-git add .
-git commit -a -m "$DATE $TIME"
-echo "pushing ..."
-git push
+cd .. && DATE=$(date -d'1 day ago' "+%Y-%m-%d") && TIME=$(date "+%H:%M:%S") && echo -e "  ____\n |\n   The upload-to-git-platform script as date-time format commit\n                                                             ____| \n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n[+] Commiting changes ..." && git add . && git commit -a -m "$DATE $TIME" && echo "[+] Pushing ..." && git push
