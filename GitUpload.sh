da6ce365a6e7c602684f4da00e9b883b2c0fdf08
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2021-07-09 15:31:48 +0800

    Update GitUpload.sh

--- GitUpload.sh
+++ GitUpload.sh
@@ -1,5 +1,12 @@
 # !/bin/bash
 # Go to https://github.com/sunbossrs/git-commit-script/ for more infos.
 
-git pull origin master && echo "\n\n[+] Repository commit:" && read -p "> " REPLY  
-git add . && git stage . && git commit -a -m "${REPLY}" && git gc && git push && echo "[+] Thank you for using this tool!"
+git pull origin master
+echo "\n\n[+] Repository commit:"
+read -p "> " REPLY  
+git add .
+git stage .
+git commit -a -m "${REPLY}"
+git gc
+git push
+echo "[+] Thank you for using this tool!"
